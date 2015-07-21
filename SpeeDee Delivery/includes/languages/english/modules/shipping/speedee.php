bce9380cf1f8c901c7e9883cc3f0ddbe16c77103
Larry Bislew <bislewl@gmail.com>
2015-07-21 16:14:28 -0500

    1 | Misspelling Correct spelling. Thanks @DrByte

---
+++ SpeeDee Delivery/includes/languages/english/modules/shipping/speedee.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  
+  Released under the GNU General Public License
+*/
+define('MODULE_SHIPPING_SPEEDEE_TEXT_TITLE', 'Spee Dee Delivery');
+define('MODULE_SHIPPING_SPEEDEE_TEXT_DESCRIPTION', 'Spee Dee Delivery Services');
+define('MODULE_SHIPPING_SPEEDEE_TEXT_WAY', 'Spee Dee Delivery - Ground');
+
